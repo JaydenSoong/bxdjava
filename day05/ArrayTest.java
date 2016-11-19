bb4d2e3b58d55fb6cfd0892367bed849f8413da4
JaydenSoong <jiandongsong@126.com>
2016-11-19 09:37:05 +0800

    增加冒泡排序算法

--- day05/ArrayTest.java
+++ day05/ArrayTest.java
@@ -8,7 +8,13 @@ public class ArrayTest {
         int[] arr = {23,25,12,3,56,89,1};
         printArr(arr);
         selectSort(arr);
+        System.out.print("选择排序算法结果：");
         printArr(arr);
+        int[] arr1 = {23,25,12,3,56,89,4};
+        printArr(arr1);
+        System.out.print("冒泡排序算法结果：");
+        bubbleSort(arr1);
+        printArr(arr1);
     }
 
     //选择排序的算法，大圈套小圈的思想，两层 for 循环
@@ -19,6 +25,19 @@ public class ArrayTest {
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
+                }
+            }
+        }
+    }
+    
+    //冒泡排序的算法，大圈套小圈的思想，两层 for 循环
+    public static void bubbleSort(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
