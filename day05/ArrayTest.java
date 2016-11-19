c310bfcf8f24b68da38b561197ad2bd095c94fd9
JaydenSoong <jiandongsong@126.com>
2016-11-19 09:52:01 +0800

    抽取置换代码，将其封装

--- day05/ArrayTest.java
+++ day05/ArrayTest.java
@@ -22,9 +22,10 @@ public class ArrayTest {
         for(int i=0; i<arr.length-1; i++){
             for(int j=i+1; j<arr.length; j++){
                 if(arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                    //int temp = arr[i];
+                    //arr[i] = arr[j];
+                    //arr[j] = temp;
+                    swap(arr, i, j);
                 }
             }
         }
@@ -35,9 +36,10 @@ public class ArrayTest {
         for(int i=0; i<arr.length-1; i++){
             for(int j=0; j<arr.length-1-i; j++){
                 if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    //int temp = arr[j];
+                    //arr[j] = arr[j+1];
+                    //arr[j+1] = temp;
+                    swap(arr, j, j+1);
                 }
             }
         }
@@ -49,5 +51,12 @@ public class ArrayTest {
             System.out.print(element + " ");
         }
         System.out.println();
+    }
+    
+    //置换部分可抽取出来，减少代码重复，提高利用性
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] =  arr[b];
+        arr[b] = temp;
     }
 }
