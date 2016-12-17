f8d4fbf6f80b329b505366e448270b47dc26d1d0
JaydenSoong <jiandongsong@126.com>
2016-12-17 10:57:08 +0800

    健壮程序，添加注释

--- OOPTest/PersonTest.java
+++ OOPTest/PersonTest.java
@@ -9,6 +9,32 @@
  */
 
 
+//描述年龄异常
+class WrongAgeException extends RuntimeException {
+    
+    WrongAgeException() {
+    
+    }
+
+    WrongAgeException(String msg) {
+        super(msg);
+    }
+}
+
+
+//描述比较对象异常
+class NullPersonException extends RuntimeException {
+    
+    NullPersonException() {
+    
+    }
+
+    NullPersonException(String msg) {
+        super(msg);
+    }
+}
+
+
 class Person {
     
     private String name;
@@ -30,7 +56,9 @@ class Person {
         return this.age;
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age){
+        if(age < 0)
+            throw new WrongAgeException("年龄不合法");
         this.name = name;
         this.age = age;
     }
@@ -40,6 +68,8 @@ class Person {
     }
 
     public void sameOne(Person p) {
+        if(p == null)
+            throw new NullPersonException("比较对象不存在");
         if (this.getAge() == p.getAge() || this.getName().equals(p.getName())) {
             System.out.println("是同一个人");
         } else {
@@ -53,7 +83,9 @@ public class PersonTest {
     public static void main (String[] args) {
         Person p1 = new Person("张三", 21);
         Person p2 = new Person("李四", 22);
+        //Person p4 = new Person("李四", -2);
         Person p3 = new Person("张三", 21);
+        //p3 = null;
 
         p1.introduce();
         p2.introduce();
