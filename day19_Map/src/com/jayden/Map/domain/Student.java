da0c6264fce7fbadc9a32dd101782c976cf1fa43
JaydenSoong <jiandongsong@126.com>
2017-03-08 16:21:52 +0800

    学生类

---
+++ day19_Map/src/com/jayden/Map/domain/Student.java
@@ -0,0 +1,66 @@
+package com.jayden.Map.domain;
+
+/**
+ * Created by DaiLiang on 2017/3/8.
+ * 学生类
+ */
+public class Student implements Comparable {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (age != student.age) return false;
+        return name != null ? name.equals(student.name) : student.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        int temp = age - ((Student)o).age;
+        return temp == 0 ? name.compareTo(((Student)o).name) : temp;
+    }
+}
