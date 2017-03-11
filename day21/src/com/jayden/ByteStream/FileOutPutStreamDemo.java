b4e3713f52f3581756363719bbcc856314184c13
JaydenSoong <jiandongsong@126.com>
2017-03-11 11:46:22 +0800

    文件输出流演示

---
+++ day21/src/com/jayden/ByteStream/FileOutPutStreamDemo.java
@@ -0,0 +1,27 @@
+package com.jayden.ByteStream;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by DaiLiang on 2017/3/11.
+ * 文件输出流演示
+ */
+public class FileOutPutStreamDemo {
+    public static void main(String[] args) throws IOException {
+//        创建 FileOutPutStream 操作的文件对象
+        File dir = new File("temp_files");
+        if (!dir.exists())
+            dir.mkdir();
+
+//        创建 FileOutPutSteam 对象
+        FileOutputStream fos = new FileOutputStream(dir + "\\fos.txt");
+
+//        调用 FileOutPutStream 对象的写方法
+        fos.write("Hello, World!".getBytes());
+
+//        释放资源
+        fos.close();
+    }
+}
