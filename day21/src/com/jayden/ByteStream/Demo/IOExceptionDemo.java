df45feea3c1b9e76ea405a60578a7366dc637fd2
JaydenSoong <jiandongsong@126.com>
2017-03-14 10:48:56 +0800

    续写、换行

--- day21/src/com/jayden/ByteStream/Demo/IOExceptionDemo.java
+++ day21/src/com/jayden/ByteStream/Demo/IOExceptionDemo.java
@@ -8,11 +8,13 @@ import java.io.FileOutputStream;
  * IOException 演示
  */
 public class IOExceptionDemo {
+    public static final String LINE_SEPARATOR = System.lineSeparator();
     public static void main(String[] args) {
         FileOutputStream ioe = null;
         try {
-            ioe = new FileOutputStream("temp_files\\IOExceptionDemo.txt");
-            ioe.write("IOException".getBytes());
+            ioe = new FileOutputStream("temp_files\\IOExceptionDemo.txt", true); //加入参数 true, 实现续写。
+            String str = LINE_SEPARATOR + "IOException";
+            ioe.write(str.getBytes());
         } catch (IOException e) {
             System.out.println(e);
         } finally {
