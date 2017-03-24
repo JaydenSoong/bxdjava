d348058cb1562b8211fe3c9a22ecf41fe5c1140f
JaydenSoong <jiandongsong@126.com>
2017-03-24 15:05:09 +0800

    正则练习——验证邮箱

--- day25/src/com/jayden/regex/Test/Test07.java
+++ day25/src/com/jayden/regex/Test/Test07.java
@@ -7,8 +7,12 @@ package com.jayden.regex.Test;
 public class Test07 {
     public static void main(String[] args) {
         String email = "jiandomgsong@126.com";
+        boolean isRight = checkEmail(email);
+        System.out.println(email + ":" + isRight);
+    }
+
+    private static boolean checkEmail(String email) {
         String regex = "\\w+@[a-zA-Z0-9]+(\\.[a-zA-Z]{2,3}){1,3}"; //真正使用的时候，可在网上搜寻相关的正则
-        boolean b = email.matches(regex);
-        System.out.println(email + ":" + b);
+        return email.matches(regex);
     }
 }
