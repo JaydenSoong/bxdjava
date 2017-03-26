199d2728690d3ce69e1192e76d2a62c17cd058a3
JaydenSoong <jiandongsong@126.com>
2017-03-26 21:27:52 +0800

    InetAdress 演示

--- day26/src/com/jayden/net/demo/InetAdressDemo.java
+++ day26/src/com/jayden/net/demo/InetAdressDemo.java
@@ -9,7 +9,13 @@ import java.net.UnknownHostException;
  */
 public class InetAdressDemo {
     public static void main(String[] args) throws UnknownHostException {
+//        获取本机 IP 信息
         InetAddress ia = InetAddress.getLocalHost();
         System.out.println(ia);
+
+//        获取网络主机信息
+        InetAddress ip = InetAddress.getByName("www.tangwai.com");
+        System.out.println(ip.getHostAddress());
+        System.out.println(ip.getHostName());
     }
 }
