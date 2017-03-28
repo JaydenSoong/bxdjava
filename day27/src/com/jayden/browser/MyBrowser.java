28a1e1a8c7c2442a99987ddadae1d127460ad332
JaydenSoong <jiandongsong@126.com>
2017-03-28 23:43:54 +0800

    模拟浏览器

---
+++ day27/src/com/jayden/browser/MyBrowser.java
@@ -0,0 +1,31 @@
+package com.jayden.browser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by SOONG on 2017/3/28.
+ * 模拟浏览器
+ */
+public class MyBrowser {
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("192.168.3.120", 8080);
+        PrintWriter pw = new PrintWriter(s.getOutputStream(),true);
+        pw.println("GET /1.html HTTP/1.1");
+        pw.println("Accept: text/html, application/xhtml+xml, image/jxr, */*");
+        pw.println("Accept-Language: zh-CN");
+        pw.println("User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko");
+        pw.println("Accept-Encoding: gzip, deflate");
+        pw.println("Host: 192.168.3.33:9090");
+        pw.println("Connection: Keep-Alive");
+
+        InputStream is = s.getInputStream();
+        byte[] buf = new byte[1024];
+        int len = is.read(buf);
+        String str = new String(buf, 0, len);
+        System.out.println(str);
+        s.close();
+    }
+}
