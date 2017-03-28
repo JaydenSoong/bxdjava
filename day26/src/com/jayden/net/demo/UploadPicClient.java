db18b496af27d043638f1bf5554b1d23177c6a65
JaydenSoong <jiandongsong@126.com>
2017-03-28 21:40:29 +0800

    图片上传客户端，上传本地图片，显示服务端反馈信息“图片上传成功”。

--- day26/src/com/jayden/net/demo/UploadPicClient.java
+++ day26/src/com/jayden/net/demo/UploadPicClient.java
@@ -11,9 +11,9 @@ public class UploadPicClient {
     public static void main(String[] args) throws IOException {
         System.out.println("图片上传客户端启动......");
 //        1、建立图片上传客户端
-        Socket s = new Socket("172.16.17.71", 10010);
+        Socket s = new Socket("192.168.80.104", 10010);
 //        2、明确上传文件（流的源文件）
-        FileInputStream fis = new FileInputStream(new File("1.jpg"));
+        FileInputStream fis = new FileInputStream(new File("3.jpg"));
 //        3、明确网络输出流
         OutputStream ops = s.getOutputStream();
 //        4、频繁读写
