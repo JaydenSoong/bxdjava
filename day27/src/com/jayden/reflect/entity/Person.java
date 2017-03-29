5724d7c50a071001a772a6b45c1f892253cd52ea
JaydenSoong <jiandongsong@126.com>
2017-03-29 11:17:07 +0800

    演示反射技术用的 Person 类

---
+++ day27/src/com/jayden/reflect/entity/Person.java
@@ -0,0 +1,18 @@
+package com.jayden.reflect.entity;
+
+/**
+ * Created by DaiLiang on 2017/3/29.
+ * 演示反射技术用的 Per：wson 类
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
